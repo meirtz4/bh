c77e9888b1e34f604081579526f528afab321606
Meir <meirtz4@gmail.com>
2022-05-18 21:09:04 +0300

    initial

---
+++ main.py
@@ -0,0 +1,92 @@
+import os
+import sys
+import datetime
+from dotenv import load_dotenv
+from openweathermap import get_forecast_per_city_per_hour, get_current_feels_like
+
+load_dotenv()
+
+UNIX_DAY=60*60*24
+
+
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+
+CITIES = [
+    City('Jerusalem', '31.7788242', '35.2257626'),
+    City('Haifa', '32.8191218', '34.9983856'),
+    City('TelAviv', '32.0852997', '34.7818064'),
+    City('Eilat', '29.5569348', '34.9497949'),
+    City('Tiberias', '32.7892439', '35.5213860')
+]
+
+
+def slice_hourly_by_days(hourly_forecast):
+    result = {}
+    today_morning = int(datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0).strftime('%s'))
+    for measurement in hourly_forecast:
+        diff_since_morning = measurement['dt'] - today_morning
+        day_index = int(diff_since_morning / UNIX_DAY)
+        if result.get(str(day_index)) is None:
+            result[str(day_index)] = []
+        result[str(day_index)].append(measurement)
+
+    return result
+
+
+def get_avg_tmp_per_day(city):
+    hourly_forecast = get_forecast_per_city_per_hour(city)
+    sliced_hourly_forecast = slice_hourly_by_days(hourly_forecast)
+    result = {
+        'name': city.name,
+        'avg_per_day': {}
+    }
+    for day_index in sliced_hourly_forecast:
+        sum_temp = sum(c['temp'] for c in sliced_hourly_forecast[day_index])
+        sum_days = len(sliced_hourly_forecast[day_index])
+        result['avg_per_day'][day_index] = sum_temp / sum_days
+
+    return result
+
+
+def get_avg_tmp_per_city_per_day():
+    result = []
+    for city in CITIES:
+        result.append(get_avg_tmp_per_day(city))
+
+    return result
+
+
+def get_lowest_humid():
+    lowest_humidity = {
+        'city': 'NA',
+        'time': 'NA',
+        'humidity': sys.maxsize,
+    }
+    for city in CITIES:
+        hourly_forecast = get_forecast_per_city_per_hour(city)
+        min_in_city = min(hourly_forecast, key=lambda x: x['humidity'])
+        if min_in_city['humidity'] < lowest_humidity['humidity']:
+            lowest_humidity = {
+                'city': city.name,
+                'time': min_in_city['dt'],
+                'humidity': min_in_city['humidity'],
+            }
+
+    return lowest_humidity
+
+
+def feels_like_rank(order_dir='acs'):
+    result = []
+    for city in CITIES:
+        result.append({
+            'name': city.name,
+            'feels_like': get_current_feels_like(city)
+        })
+
+    result.sort(key=lambda x: x['feels_like'], reverse=(order_dir == 'desc'))
+    return result
---
+++ openweathermap.py
@@ -0,0 +1,45 @@
+
+import os
+import requests
+import redis
+import pickle
+
+cache = redis.Redis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'), password=os.getenv('REDIS_PASSWORD'))
+
+CACHE_VERSION=os.getenv('CACHE_VERSION')
+GEO_BASE_URL=os.getenv('GEO_BASE_URL')
+DATA_ONE_CALL_BASE_URL=os.getenv('DATA_ONE_CALL_BASE_URL')
+DATA_WEATHER_BASE_URL=os.getenv('DATA_WEATHER_BASE_URL')
+API_ID=os.getenv('API_ID')
+CACHE_EXPIRATION=3600
+
+
+def _get_city_coordinates(_city):
+    url = f'{GEO_BASE_URL}q={_city}&limit=1&{API_ID}'
+    response = requests.get(url).json()
+    response_in_israel = [x for x in response if x['country'] == 'IL'][0]
+    return response_in_israel['lat'], response_in_israel['lon']
+
+
+def get_forecast_per_city_per_hour(city):
+    key = f'get_forecast_per_city_per_hour_{city.name}_{CACHE_VERSION}'
+    cache_response = cache.get(key)
+    if cache_response:
+        return pickle.loads(cache_response)['hourly']
+    else:
+        url = f'{DATA_ONE_CALL_BASE_URL}lat={city.lat}&lon={city.lon}&exclude=current,minutely,daily,alerts&units=metric&{API_ID}'
+        response = requests.get(url).json()
+        cache.set(key, pickle.dumps(response), ex=CACHE_EXPIRATION)
+        return response['hourly']
+
+
+def get_current_feels_like(city):
+    key = f'get_current_feels_like_{city.name}_{CACHE_VERSION}'
+    cache_response = cache.get(key)
+    if cache_response:
+        return pickle.loads(cache_response)['main']['feels_like']
+    else:
+        url = f'{DATA_WEATHER_BASE_URL}lat={city.lat}&lon={city.lon}&{API_ID}'
+        response = requests.get(url).json()
+        cache.set(key, pickle.dumps(response), ex=CACHE_EXPIRATION)
+        return response['main']['feels_like']
